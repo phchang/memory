f86fb3449df553b66b131b260d51f462912e3ab5
Paul Chang <paul@phc.io>
2014-10-27 05:52:35 -0700

    removing unused code from seed project

--- app/app.js
+++ app/app.js
@@ -3,8 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.version'
+  'myApp.view1'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
